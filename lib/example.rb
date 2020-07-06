c602cecb2a98f0e4549129ae4d4df44cf9be4186
lizbur10 <liz.burton147@gmail.com>
2020-07-06 08:50:55 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,11 @@
 # Use this space to code along with the readme
+def say_hello_ten_times
+    phrase = "Hello World!"
+    i = 0
+    while i < 10 do
+        puts phrase
+        i += 1
+    end 
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_ten_times
